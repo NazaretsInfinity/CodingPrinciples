482c07204661f2cc283b0a42d4b5aa27f2f7b8a9
NazaretsInfinity <157612924+NazaretsInfinity@users.noreply.github.com>
2025-06-23 20:45:20 +0500

    added project

---
+++ InversionOfControl_MicrosoftDI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InversionOfControl_MicrosoftDI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
